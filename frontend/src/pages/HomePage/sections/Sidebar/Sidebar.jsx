edc52d0d9861fe173e45a996978a9238c50409d0
TomasCastano <tomascastanochica@gmail.com>
2025-05-26 15:25:44 -0500

    Make the sidebar responsive

--- frontend/src/pages/HomePage/sections/Sidebar/Sidebar.jsx
+++ frontend/src/pages/HomePage/sections/Sidebar/Sidebar.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useState } from 'react'
+import { useNavigate, Link, useLocation } from 'react-router-dom'
+import { useAuth } from '../../../../providers/AuthProvider'
 import logo from '../../../../assets/images/logo.webp'
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
@@ -6,13 +8,15 @@ import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined
 import MapOutlinedIcon from '@mui/icons-material/MapOutlined'
 import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined'
 import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined'
-import { useNavigate, Link, useLocation } from 'react-router-dom'
-import { useAuth } from '../../../../providers/AuthProvider'
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded'
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
 
 const Sidebar = () => {
 
     const navigate = useNavigate()
     const { logout, user } = useAuth()
+
+    const [open, setOpen] = useState(false)
 
     const isAdmin = user?.rol_id === 1
 
@@ -24,8 +28,24 @@ const Sidebar = () => {
         navigate('/login')
     }
 
+    const handleOpenSidebar = () => {
+        document.querySelector('nav').classList.toggle('hidden')
+        document.querySelector('nav').classList.toggle('flex')
+        setOpen(!open)
+    }
+
     return (
-        <nav className="w-[200px] h-[100dvh] bg-white border-r border-gray-200 flex flex-col justify-between">
+        <>
+        <div className={'md:hidden fixed z-50 ' + (open ? 'top-3 left-[210px]' : 'top-3 left-3')}>
+            <button
+                className={"bg-white text-gray-900 p-2 rounded hover:bg-gray-200 w-fit flex items-center gap-2 text-sm cursor-pointer transition-colors " + (open ? 'bg-white' : 'bg-white/50')}
+                onClick={handleOpenSidebar}
+            >
+                {open ? <CloseRoundedIcon fontSize="medium" /> : <MenuRoundedIcon fontSize="medium" />}
+            </button>
+        </div>
+        <div className={"absolute md:hidden z-10 h-[100dvh] w-[100vw] bg-gray-900/50 " + (open ? 'block' : 'hidden')} />
+        <nav className="fixed md:relative w-[200px] h-[100dvh] bg-white hidden md:flex border-r border-gray-200 flex-col justify-between z-40">
             <header className="px-4 py-6 flex flex-col items-center justify-center border-b border-gray-200">
                 <Link to="/home" className="flex flex-col items-center gap-1">
                     <img className='w-9 h-auto object-contain' src={logo} alt="logo" />
@@ -51,6 +71,7 @@ const Sidebar = () => {
                 </button>
             </footer>
         </nav>
+        </>
     )
 }
 
